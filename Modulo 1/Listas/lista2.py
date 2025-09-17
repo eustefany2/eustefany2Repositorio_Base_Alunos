3b9c69f97d7bad7333d5603dd2da02a0e4bd1ebb
Stefany <nunes08stefany@gmail.com>
2025-09-17 09:08:34 -0300

    Update and rename Orientações.txt to lista2.py

---
+++ Modulo 1/Listas/lista2.py
@@ -0,0 +1,10 @@
+nomes = ["kethleen", "luiz", "noah", "paulo", "vanessa", "Apolo"]
+print(nomes[0])
+print(nomes[1])
+print(nomes[2])
+
+nomes[1] = "alex"
+
+print(nomes[0])
+print(nomes[1])
+print(nomes[2])
