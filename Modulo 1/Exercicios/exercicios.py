5196b3a5ca4d4a0baaaa0b864f34d0d9b50731bc
Stefany <nunes08stefany@gmail.com>
2025-09-17 09:29:44 -0300

    Update and rename Orientações.txt to exercicios.py

---
+++ Modulo 1/Exercicios/exercicios.py
@@ -0,0 +1,6 @@
+def aneis_prata():
+    print("comprei aneis pratas")
+def aneis_dourados():
+    print("vou comprar aneis dourados")
+
+    opcao = int(input("digite qual deseja comprar: \n 1- aneis pratas \n 2- aneis dourados"))
